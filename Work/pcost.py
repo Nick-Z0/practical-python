daf77ded9ac3117aa90a18244ff5dfd19022731c
Nick Papanikolaou <ns.papanikolaou1@gmail.com>
2022-07-10 00:10:41 +0300

    Exercise 1.27 pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+cost = 0
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f)
+for line in f:
+    row = line.split(',')
+    cost = cost + int(row[1]) * float(row[2])
+f.close()
+print(cost)
