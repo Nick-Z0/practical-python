2c1b99179fcb405c8d486abc8fdd3d2196c21db1
Nick Papanikolaou <ns.papanikolaou1@gmail.com>
2022-07-10 15:12:29 +0300

    Exercise 1.30 pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,14 @@
 # pcost.py
 #
-# Exercise 1.27
-cost = 0
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f)
-for line in f:
-    row = line.split(',')
-    cost = cost + int(row[1]) * float(row[2])
-f.close()
-print(cost)
+# Exercise 1.30
+def portfolio_cost(filename):
+    cost_temp = 0
+    f = open(filename, 'rt')
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        cost_temp = cost_temp + int(row[1]) * float(row[2])
+    f.close()
+    return cost_temp
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
