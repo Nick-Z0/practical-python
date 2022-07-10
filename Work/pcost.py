9e29294bc04d32083b9bd9c9fb5751722ac356a4
Nick Papanikolaou <ns.papanikolaou1@gmail.com>
2022-07-10 15:22:43 +0300

    Exercise 1.31 pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,16 @@
 # pcost.py
 #
-# Exercise 1.30
+# Exercise 1.31
 def portfolio_cost(filename):
     cost_temp = 0
     f = open(filename, 'rt')
     headers = next(f)
     for line in f:
-        row = line.split(',')
-        cost_temp = cost_temp + int(row[1]) * float(row[2])
+        try:
+            row = line.split(',')
+            cost_temp = cost_temp + int(row[1]) * float(row[2])
+        except ValueError:
+            print("Couldn't parse", line) 
     f.close()
     return cost_temp
 cost = portfolio_cost('Data/portfolio.csv')
