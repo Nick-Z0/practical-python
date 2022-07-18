2eac90ee9d11cb0981dd23e1b9bcf586fe5c3143
Nick Papanikolaou <ns.papanikolaou1@gmail.com>
2022-07-18 16:15:39 +0300

    Exercise 2.5 report.py

--- Work/report.py
+++ Work/report.py
@@ -1,16 +1,25 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.5
 import csv
 
 def read_portfolio(filename):
-    '''Returns a list of the portfolio file'''
+    '''
+    Reads a stock portfolio file and returns a list of dictionaries of the 
+portfolio file with keys: name, shares, price
+    '''
     portfolio = []
-
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
+
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
-            portfolio.append(holding)
+            record = dict(zip(headers, row))
+            stock = {
+                'name'   : record['name'],
+                'shares' : int(record['shares']),
+                'price'  : float(record['price'])
+            }
+            portfolio.append(stock)
+
     return(portfolio)
